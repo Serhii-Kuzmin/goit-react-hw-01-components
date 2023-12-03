f313f814ada50929433f73e8711d93394e465671
Serhii-Kuzmin <seryy8248@gmail.com>
2023-12-03 21:58:51 +0100

    Create-TransactionHistory

---
+++ src/components/TransitionHistory/TransactionHistory.jsx
@@ -0,0 +1,29 @@
+import css from './TransactionHistory.module.css';
+import PropTypes from 'prop-types';
+import { Transactions } from 'components/Transactions/Transactions';
+
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table className={css.transitonHistory}>
+      <thead>
+        <th className={css.row}>
+          <th className={css.cell}>Type</th>
+          <th className={css.cell}>Amount</th>
+          <th className={css.cell}>Currency</th>
+        </th>
+      </thead>
+      <Transactions items={items} />
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+};
