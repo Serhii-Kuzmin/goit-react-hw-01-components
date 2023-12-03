e29918f509483fa63ebac7d612aea2915fc5001c
Serhii-Kuzmin <seryy8248@gmail.com>
2023-12-03 21:45:25 +0100

    Create-StatsList

---
+++ src/components/StatsList/StatsList.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import css from './StatsList.module.css';
+import { makeGreen } from './makeGreen';
+
+export const StatsList = ({ stats }) => {
+  return (
+    <ul className={css.statList}>
+      {stats.map(({ id, label, percentage }) => {
+        return (
+          <li
+            className={css.item}
+            key={id}
+            style={{ backgroundColor: `${makeGreen()}` }}
+          >
+            <span className="label">{label}</span>
+            <span className="percentage">{percentage}%</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+StatsList.propTypes = {
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    }).isRequired
+  ).isRequired,
+};
---
+++ src/components/StatsList/makeGreen.js
@@ -0,0 +1,3 @@
+export function makGreen() {
+  return `rgb(0, ${Math.floor(Math.random() * 156 + 100)}, 0)`;
+}
